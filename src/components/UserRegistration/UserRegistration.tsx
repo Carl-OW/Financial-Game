feffad7d14dba99817bb901c6361137fa7df6e2d
Glenruben Larsen <gle@ssb.no>
2024-09-17 15:49:07 +0200

    Add leaderboard, not finished

--- src/components/UserRegistration/UserRegistration.tsx
+++ src/components/UserRegistration/UserRegistration.tsx
@@ -11,7 +11,7 @@ function handleSubmit(event: FormEvent<HTMLFormElement>) {
     [formData.elements.email.value]: {
       name: formData.elements.name.value,
       email: formData.elements.email.value,
-      score: 9000,
+      score: formData.elements.score.value,
     },
   });
 }
@@ -21,7 +21,7 @@ const UserRegistration: React.FC = () => {
     <div className="formContainer">
       <form id="signupForm" onSubmit={handleSubmit}>
         <input type="text" id="name" name="name"></input>
-        <input type="email" id="email" name="email"></input>
+        <input type="text" id="email" name="email"></input>
         <input type="number" id="score" name="score"></input>
         <button>HELO</button>
       </form>
