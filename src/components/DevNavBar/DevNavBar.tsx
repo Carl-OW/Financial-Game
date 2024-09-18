8c40c8b88853155962b1fd537a7f9eee5ba92e22
Glenruben Larsen <gle@ssb.no>
2024-09-18 15:41:20 +0200

    Add Numberguess to nav bar

--- src/components/DevNavBar/DevNavBar.tsx
+++ src/components/DevNavBar/DevNavBar.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import { GraphView } from "../GraphView/GraphView";
-import UserRegistration from "../UserRegistration/UserRegistration";
-import Leaderboard from "../Leaderboard/Leaderboard";
-import Admin from "../Admin/Admin";
-import Game from "../Game/Game";
+import { useState } from 'react';
+import { GraphView } from '../GraphView/GraphView';
+import UserRegistration from '../UserRegistration/UserRegistration';
+import Leaderboard from '../Leaderboard/Leaderboard';
+import Admin from '../Admin/Admin';
+import Game from '../Game/Game';
+import NumberGuess from '../NumberGuess/NumberGuess';
 
 function DevNavBar() {
   const [view, setView] = useState<string | null>(null);
@@ -17,26 +18,30 @@ function DevNavBar() {
       {/* Only render the navbar if no view is selected */}
       {!view && (
         <div className="dev-navbar">
-          <button onClick={() => handleClick("graphview")}>Graph View</button>
-          <button onClick={() => handleClick("quizview")}>Quiz View</button>
-          <button onClick={() => handleClick("UserRegistration")}>
+          <button onClick={() => handleClick('graphview')}>Graph View</button>
+          <button onClick={() => handleClick('quizview')}>Quiz View</button>
+          <button onClick={() => handleClick('numberview')}>
+            Number Guesser View
+          </button>
+          <button onClick={() => handleClick('UserRegistration')}>
             User Registration
           </button>
-          <button onClick={() => handleClick("Leaderboard")}>
+          <button onClick={() => handleClick('Leaderboard')}>
             Leaderboard
           </button>
-          <button onClick={() => handleClick("Admin")}>Admin</button>
-          <button onClick={() => handleClick("Game")}>Game</button>
+          <button onClick={() => handleClick('Admin')}>Admin</button>
+          <button onClick={() => handleClick('Game')}>Game</button>
         </div>
       )}
 
       {/* Render the selected view */}
       <div className="view-container">
-        {view === "graphview" && <GraphView />}
-        {view === "UserRegistration" && <UserRegistration />}
-        {view === "Leaderboard" && <Leaderboard />}
-        {view === "Admin" && <Admin />}
-        {view === "Game" && <Game />}
+        {view === 'graphview' && <GraphView />}
+        {view === 'numberview' && <NumberGuess />}
+        {view === 'UserRegistration' && <UserRegistration />}
+        {view === 'Leaderboard' && <Leaderboard />}
+        {view === 'Admin' && <Admin />}
+        {view === 'Game' && <Game />}
       </div>
     </>
   );
